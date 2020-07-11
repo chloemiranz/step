47f6c95522263d1b4162fc7fa6c82413920a16af
Chloe <chloemiranz@gmail.com>
2020-07-11 23:07:11 +0000

    Added blobstore

---
+++ portfolio/src/main/java/com/google/sps/servlets/BlobstoreServlet.java
@@ -0,0 +1,51 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*; 
+
+
+/**
+ * This servlet prints out the HTML for the homepage. You wouldn't do this in a real codebase, but
+ * this is meant to demonstrate getting a Blobstore URL and using it in a form to allow a user to
+ * upload a file.
+ */
+@WebServlet("/blobstore")
+public class BlobstoreServlet extends HttpServlet {
+
+  /**
+   * Returns HTML that contains a form. The form submits to Blobstore, which redirects to our
+   * /my-form-handler, which is handled by FormHandlerServlet.
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    String uploadUrl = blobstoreService.createUploadUrl("/form-handler");
+
+    response.setContentType("text/html");
+    response.getWriter().println(uploadUrl);
+    }
+}
+
+
