9487d86c0a1677e206dd92075dbb6cf83a027dd8
Chloe <chloemiranz@gmail.com>
2020-07-20 19:45:00 +0000

    Fixed again

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -81,11 +81,11 @@ function getComments() {
           const fetches = [];
           for (let i = 0; i < max; i++) {
             if (comments[i].hasOwnProperty('blobKey') &&
-            comments[i].blobKey !== 'noBlob') {
+                comments[i].blobKey !== 'noBlob') {
               fetches.push(fetch('/getBlob?blobKey=' + comments[i].blobKey)
-                  .then((imgBlob) => {
-                    comments[i].url = imgBlob.url;
-                  }));
+                                .then((imgBlob) => {
+                                comments[i].url = imgBlob.url;
+                                }));
             }
           }
           Promise.all(fetches).then(() => {
